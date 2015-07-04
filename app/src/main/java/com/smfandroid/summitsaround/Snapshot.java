7195a1e163710355c78c0322bcc962e8829b6b94
Simon Marache-francisco <git@marache.net>
2015-07-04 18:26:25 +0200

    images displayed in galery

--- app/src/main/java/com/smfandroid/summitsaround/Snapshot.java
+++ app/src/main/java/com/smfandroid/summitsaround/Snapshot.java
@@ -1,16 +1,24 @@
 package com.smfandroid.summitsaround;
 
+import android.content.ContentValues;
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.hardware.Camera;
 import android.graphics.Canvas;
+import android.net.Uri;
 import android.os.Environment;
+import android.provider.MediaStore;
+import android.text.style.TtsSpan;
 import android.util.Log;
 import android.widget.Toast;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 
 public class Snapshot implements Camera.ShutterCallback, Camera.PictureCallback {
@@ -43,17 +51,35 @@ public class Snapshot implements Camera.ShutterCallback, Camera.PictureCallback 
 
         Log.i(TAG, "onPictureTaken called");
         String path = Environment.getExternalStorageDirectory().toString();
-        OutputStream fOut = null;
-        File file = new File(path, "summitsAround.jpg");
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmss");
+        String currentDateandTime = sdf.format(new Date());
+
+        File file = new File(path, "summitsAround" + currentDateandTime + ".jpg");
+
         try {
-            fOut = new FileOutputStream(file);
+            FileOutputStream fOut = new FileOutputStream(file);
             b.compress(Bitmap.CompressFormat.JPEG, 95, fOut);
+            addImageToGallery(mCamView.getContext(), file.getAbsolutePath(), "summitAround", "summitAround");
         } catch(IOException exc)
         {
             Toast.makeText(mCamView.getContext(), "Impossible to save image", Toast.LENGTH_LONG);
         }
         mPrev.onResume();
     }
+
+    // from http://stackoverflow.com/questions/21759476/android-save-bitmap-to-gallery-time-created-wrong
+    public static Uri addImageToGallery(Context context, String filepath, String title, String description) {
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.TITLE, title);
+        values.put(MediaStore.Images.Media.DESCRIPTION, description);
+        values.put(MediaStore.Images.Media.DATE_TAKEN, System.currentTimeMillis());
+        values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
+        values.put(MediaStore.MediaColumns.DATA, filepath);
+
+        return context.getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+    }
+
 }
 
 
